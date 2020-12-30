8edf937e8551f53d1d93ce238696dda7e5c8579f
kimyb8870 <qhd0084@gmail.com>
2020-12-30 22:39:26 +0900

    Add: modal for sign up with open button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./firebase";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false); //tracking state of modal is open or not
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -19,12 +47,21 @@ function App() {
 
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>sign up here</h2>
+          {/* <SimpleModal /> */}
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
         />
       </div>
+
+      <Button onClick={() => setOpen(true)}>Sign up</Button>
 
       {/* test post below*/}
       {posts.map(({ id, post: { username, imageUrl, caption } }) => (
