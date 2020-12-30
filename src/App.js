b6bc918f08dfe0472cdacb932f5aa8df9521f296
kimyb8870 <qhd0084@gmail.com>
2020-12-30 16:00:25 +0900

    Remove: hotfix posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "test_name1",
+      imageUrl:
+        "https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80",
+      caption: "test caption",
+    },
+    {
+      username: "test_name2",
+      imageUrl:
+        "https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+      caption: "teeessstt caption",
+    },
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -12,32 +28,9 @@ function App() {
       </div>
 
       {/* test post below*/}
-      <Post
-        username="test_name1"
-        imageUrl="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80"
-        // this url is for test
-        caption="test caption"
-      />
-      <Post
-        username="test_name2"
-        imageUrl="https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
-        // this url is for test
-        caption="teeessstt caption"
-      />
-      <Post
-        username="test_name3"
-        imageUrl="https://images.unsplash.com/photo-1608585269273-5cacd36870f5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-        // this url is for test
-        caption="teeessstt caption!"
-      />
-      <Post />
-      <Post />
-      {/* Posts */}
-      {/* Posts */}
-      {/* Posts */}
-      {/* Posts */}
-      {/* Posts */}
-      {/* Posts */}
+      {posts.map(({ username, imageUrl, caption }) => (
+        <Post username={username} imageUrl={imageUrl} caption={caption} />
+      ))}
     </div>
   );
 }
