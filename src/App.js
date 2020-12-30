6c33cb4dc9563024f97b06ab2082bb962853ed33
kimyb8870 <qhd0084@gmail.com>
2020-12-30 12:20:45 +0900

    Feature: app__header logo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Instagram clone</h1>
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+        />
+      </div>
+
+      {/* Posts */}
+      {/* Posts */}
+      {/* Posts */}
+      {/* Posts */}
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
