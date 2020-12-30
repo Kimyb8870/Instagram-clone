b767316536fa24a648ba5dc84d448cbedc088e7b
kimyb8870 <qhd0084@gmail.com>
2020-12-31 02:58:57 +0900

    Feature: login button, logout button

--- src/App.js
+++ src/App.js
@@ -29,20 +29,27 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function App() {
-  const [posts, setPosts] = useState([]); //for load posts from db
-  const [open, setOpen] = useState(false); //tracking state of modal is open or not
-  const classes = useStyles(); //for styleing modal
-  const [modalStyle] = useState(getModalStyle); //for styling modal
+  const [posts, setPosts] = useState([]);
+  //for load posts from db
+  const [open, setOpen] = useState(false);
+  //manage state of sign up modal is opened or not
+  const classes = useStyles();
+  //for styleing modal
+  const [modalStyle] = useState(getModalStyle);
+  //for styling modal
+  const [openSignIn, setOpenSignIn] = useState(false);
+  //manage state of sign in(just log in) modal is opened or not
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
+  //for detect changes of auth
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         //user has logged in
-        // console.log(authUser);
+        console.log(authUser);
         setUser(authUser);
       } else {
         //user has logged out
@@ -51,6 +58,7 @@ function App() {
     });
   }, [user, username]);
 
+  //for load posts from database
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
       setPosts(
@@ -80,6 +88,17 @@ function App() {
     setOpen(false);
   };
 
+  const signin = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+
+    setEmail("");
+    setPassword("");
+    setOpenSignIn(false);
+  };
+
   return (
     <div className="app">
       <Modal open={open} onClose={() => setOpen(false)}>
@@ -88,7 +107,6 @@ function App() {
             <center>
               <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" />
             </center>
-
             <Input
               type="text"
               placeholder="username"
@@ -120,6 +138,36 @@ function App() {
         </div>
       </Modal>
 
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" />
+            </center>
+
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button type="submit" onClick={signin}>
+              Sign up
+            </Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -127,7 +175,15 @@ function App() {
         />
       </div>
 
-      <Button onClick={() => setOpen(true)}>Sign up</Button>
+      {/* for deal with log in, sign up or sign out */}
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Login</Button>
+          <Button onClick={() => setOpen(true)}>Sign up</Button>
+        </div>
+      )}
 
       {/* test post below*/}
       {posts.map(({ id, post: { username, imageUrl, caption } }) => (
