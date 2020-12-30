40e7ee8327ffb10532528cbd59addb0606fd7c16
kimyb8870 <qhd0084@gmail.com>
2020-12-30 15:40:20 +0900

    Add: test posts

--- src/App.js
+++ src/App.js
@@ -10,7 +10,26 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
         />
       </div>
-      <Post />
+
+      {/* test post below*/}
+      <Post
+        username="test_name1"
+        imageUrl="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80"
+        // this url is for test
+        caption="test caption"
+      />
+      <Post
+        username="test_name2"
+        imageUrl="https://images.unsplash.com/photo-1608549036505-ead5b1de5417?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+        // this url is for test
+        caption="teeessstt caption"
+      />
+      <Post
+        username="test_name3"
+        imageUrl="https://images.unsplash.com/photo-1608585269273-5cacd36870f5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+        // this url is for test
+        caption="teeessstt caption!"
+      />
       <Post />
       <Post />
       {/* Posts */}
--- src/Post.js
+++ src/Post.js
@@ -2,20 +2,18 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Post.css";
 
-const Post = () => {
+const Post = ({ username, caption, imageUrl }) => {
   return (
     <div className="post">
       <div className="post__header">
         <Avatar className="post__avatar" alt="kimyb" />
-        <h3>Dummy name</h3>
+        <h3>{username}</h3>
       </div>
 
-      <img
-        className="post__image"
-        src="https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=80"
-      />
+      <img className="post__image" src={imageUrl} />
       <h4 className="post__text">
-        <strong>dummy username:</strong> dummy caption
+        <strong>{username}</strong>
+        &nbsp;{caption}
       </h4>
     </div>
   );
